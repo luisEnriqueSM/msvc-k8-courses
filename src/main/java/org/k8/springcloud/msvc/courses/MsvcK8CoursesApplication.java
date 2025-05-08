34deaf415957339c4763aa79f4e59ce7cd1a4dde
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-05-08 14:04:05 -0600

    First commit

---
+++ src/main/java/org/k8/springcloud/msvc/courses/MsvcK8CoursesApplication.java
@@ -0,0 +1,13 @@
+package org.k8.springcloud.msvc.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcK8CoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcK8CoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/k8/springcloud/msvc/courses/msvc_k8_courses/MsvcK8CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.k8.springcloud.msvc.courses.msvc_k8_courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcK8CoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
